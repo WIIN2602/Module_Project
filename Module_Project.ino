87a9e46d26412603d7b97168551db384bfcb0151
WIIN2602 <128226264+WIIN2602@users.noreply.github.com>
2024-06-21 18:58:37 +0700

    Update Module_Project.ino

--- Module_Project.ino
+++ Module_Project.ino
@@ -47,15 +47,26 @@ void loop() {
   http.addHeader("Content-Type", "application/x-www-form-urlencoded");
   
   int httpCode = http.POST(postData);
-  String payload = http.getString();
-  
+  String payload = "";
+
+  if(httpCode > 0) {
+    // file found at server
+    if(httpCode == HTTP_CODE_OK) {
+      String payload = http.getString();
+      Serial.println(payload);
+    } else {
+      // HTTP header has been send and Server response header has been handled
+      Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+    }
+  } else {
+    Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+  }
+  http.end();
   Serial.print("URL : "); Serial.println(URL); 
   Serial.print("Data: "); Serial.println(postData);
   Serial.print("httpCode: "); Serial.println(httpCode);
   Serial.print("payload : "); Serial.println(payload);
   Serial.println("--------------------------------------------------");
-  
-  http.end();
   
   delay(5000);
 }
